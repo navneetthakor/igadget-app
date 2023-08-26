cb7633df567eb61a1ac57af66d821167b074bf6b
navneetthakor <tnavneer8628@gmail.com>
2023-08-26 17:56:51 +0530

    header styling completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,9 @@ import '../css/desktop.css';
 
 // logo importing 
 import logo from '../photos/igadgetnobg.png';
+import searchicon from '../photos/searchIcon.svg';
+import favorite from '../photos/favorite.png';
+import shoppingcart from '../photos/shoppingcart.png';
 
 
 export default function Navbar() {
@@ -15,9 +18,19 @@ export default function Navbar() {
         <div id="logodiv">
           <img src={logo} alt=""/>
         </div>
+
         {/* search-bar division  */}
+        <div id='searchdiv'>
+          <input type='text' id='searchbar' name='searchbar' placeholder='search gadgets' />
+          <button type='btn'><img src={searchicon} alt=''/></button>
+        </div>
+
 
         {/* icon devision  */}
+        <div id='headicon'>
+          <button type='btn'><img src={favorite} alt=''/></button>
+          <button type='btn'><img src={shoppingcart} alt=''/></button>
+        </div>
 
       </header>
     </>
