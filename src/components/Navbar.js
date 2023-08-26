f601bbb7b77ca8b818c0694c6df6c884ac120a20
navneetthakor <tnavneer8628@gmail.com>
2023-08-26 13:47:06 +0530

    navbar logo added with css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+// css file styling defined 
+import '../css/desktop.css';
+
+// logo importing 
+import logo from '../photos/igadgetnobg.png';
+
 
 export default function Navbar() {
   return (
     <>
-      
+    {/* this is navbar which always remian present on all the pages  */}
+      <header>
+        {/* logo division */}
+        <div id="logodiv">
+          <img src={logo} alt=""/>
+        </div>
+        {/* search-bar division  */}
+
+        {/* icon devision  */}
+
+      </header>
     </>
   )
 }
