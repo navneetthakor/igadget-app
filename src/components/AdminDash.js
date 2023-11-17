e0fba57760e9864c5b5f8f8ca6a01193a793fa45
navneetthakor <tnavneer8628@gmail.com>
2023-11-18 00:34:14 +0530

    admin login + authtoken manag complt

--- src/components/AdminDash.js
+++ src/components/AdminDash.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, {useState } from 'react'
 import AdminLogin from './AdminLogin'
 
 export default function AdminDash() {
+    const [next, setNext] = useState(false);
+    
+    const handleLogout = () =>{
+        localStorage.removeItem('admin');
+        setNext(false)
+    }
+
   return (
     <>
-    <AdminLogin/>
-    <h1>Welcome to dashboard</h1>
+
+ 
+    {next ? 
+    <h1>Welcome to dashboard</h1>: <AdminLogin iteam={{next,setNext}}/>
+    }
+
+    <button className='PrimButton' onClick={handleLogout}>logout</button>
       
     </>
   )
--- src/components/AdminLogin.js
+++ src/components/AdminLogin.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Form } from 'react-router-dom'
 
 export default function AdminLogin(props) {
-    const {setNext} = props;
-    const [email,setEmail] = useState("");
+    const { setNext } = props.iteam;
+    const [email, setEmail] = useState("");
     const [passwd, setPasswd] = useState("");
 
-    const emailChange = (event) =>{
+    const emailChange = (event) => {
         setEmail(event.target.value);
     }
 
@@ -15,18 +15,62 @@ export default function AdminLogin(props) {
     }
 
 
-    const findAdmin = (formData) => {
-        
+    const findAdmin = async (formData) => {
+        // api call
+        const url =
+            "http://localhost:5000/storeadmin/login";
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({"email":email, "password":passwd})
+        });
+        const data = await response.json();
+
+        localStorage.setItem('admin', data.authtoken);
+        // console.log(data);
+
+        if(data.signal === "green")
+        setNext(true);
+        else
+        alert("Invalid Credentials");
 
     }
-  return (
-    <div>
-        <form onSubmit={findAdmin}>
-            <input type='txt' name='email' onChange={emailChange} placeholder='email'/>
-            <input type='password' name='password' onChange={passwdChange} placeholder='password'/>
-            <button type='submit'> Search </button>
-        </form>
-      
-    </div>
-  )
+
+
+    const autoLogin = async () => {
+        const url =
+            "http://localhost:5000/storeadmin/getadmin";
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "authtoken": localStorage.getItem('admin')
+            }
+        });
+        const data = await response.json();
+        console.log(data)
+        if(data.signal === "green")
+        setNext(true);
+
+    }
+
+    useEffect(()=>{
+        console.log("admin")
+        if(localStorage.getItem('admin')) autoLogin();
+    })
+
+
+    return (
+        <div className='flexCenter'>
+            <div className='admlgnContainer flexCol'>
+                <p className='PrimHead'>Welcome Admin</p>
+                <input type='txt' name='email' onChange={emailChange} placeholder='email' />
+                <input type='password' name='password' onChange={passwdChange} placeholder='password' />
+                <button type='submit' onClick={findAdmin} className='PrimButton'> Submit </button>
+            </div>
+
+        </div>
+    )
 }
