34f2f2d7c19f499912a2d99d5bcda54a94305fd1
navneetthakor <tnavneer8628@gmail.com>
2023-12-16 17:51:00 +0530

    '/prods' completed with css

--- src/components/Prods.js
+++ src/components/Prods.js
@@ -1,5 +1,6 @@
 // this is to load products searched in searchbar(present in Navbar)
 import React, { useContext} from 'react'
+import Iteam from './Iteam'
 
 // importing context 
 import CommonContext from '../contexts/CommonContext'
@@ -9,47 +10,15 @@ export default function Prods() {
 
     let {common} = useContext(CommonContext);
     
-    const loadData = common?.map((prodp) => {
-        return (
-            <div className="FCart flexRow">
-            <div className="FCartProd flexRow" style={{ width: "50%", textAlign: "center" }}>
-              <img
-                src={`http://localhost:5000/${prodp.images[0]}`.replace(/\\/g, "/")}
-                alt=""
-              />
-              <div>{prodp.title}</div>
-            </div>
-            <div className="FCartExt" style={{ width: "15%", textAlign: "center" }}>{prodp.ct}</div>
-            <div className="FCartExt" style={{ width: "15%", textAlign: "center" }}>{prodp.price * prodp.ct}</div>
-            <button className="PrimButton">
-              Remove
-            </button>
-    
-          </div>
-        );
-      });
   return (
     <>
-           <Navbar/>
-      <h1 className="PrimHead"> Your Cart</h1>
-      <div className="CartMain flexCol">
-        {/* to add headings  */}
-        <div className="CartHead flexRow" style={{fontWeight: "bold"}}>
-          <div id="ptitle" style={{ width: "50%", textAlign: "center" }}>
-            Product
-          </div>
-          <div id="pqty" style={{ width: "15%", textAlign: "center" }}>
-            Quantity
-          </div>
-          <div id="price" style={{ width: "15%", textAlign: "center" }}>
-            Price
-          </div>
-        </div>
-        {loadData}
-      </div>
+       <Navbar/>
 
-      {/* to proceed for chekout  */}
-      <button className="PrimButton" style={{marginLeft: "48vw"}}>Chekout</button>
+       <div id='prodsContainer'>
+        {common?.map((prod)=>{
+          return <Iteam key={prod._id} iteam={prod}/>
+        })}
+       </div>
       
     </>
   )
