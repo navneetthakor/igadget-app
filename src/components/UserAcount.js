6d1283acdd2e4f7c8e20a4063142485958a8d128
navneetthakor <tnavneer8628@gmail.com>
2023-12-17 21:28:43 +0530

    fav -> cart data transfer + sample UserAcount.js

---
+++ src/components/UserAcount.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// importing photo (this is just for sample)
+import myprofile from '../photos/myprofile.jpg';
+
+export default function UserAcount() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
