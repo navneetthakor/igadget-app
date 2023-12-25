916fd2eb6264724b3b3b4b09fdec3429bfdd15ae
navneetthakor <tnavneer8628@gmail.com>
2023-12-26 00:01:00 +0530

    cart and fav completed

--- src/components/Favourite.js
+++ src/components/Favourite.js
@@ -29,30 +29,34 @@ const handleAddToCart = () =>{
   // function to display all the iteams that are in the cart
   const favs = product?.map((prodp) => {
     return (
-      <div className="fCart flexRow">
-        <div className="cartIteamImg">
+      <div className="fCart flexRow FlexCol">
+        <div className="cartIteamImg ">
           <img
             src={`${process.env.REACT_APP_MY_IP}/${prodp.images[0]}`.replace(/\\/g, "/")}
             alt=""
           />
         </div>
 
-        <div className="cartIteamContent flexRow">
-          <div className="cartIteamTitle flexCol">
+        <div className="CartContent cartIteamContent flexRow">
+          <div className="CartContentIteam cartIteamTitle flexCol">
             <h2 className="checkoutSecHeading">{prodp.title}</h2>
             <div>height: {prodp.height}</div>
             <div>width: {prodp.width}</div>
           </div>
 
-          <div className="cartIteamTitle flexCol">
+          <div className="CartContentIteam cartIteamTitle flexCol">
             <h2 className="fCartExt checkoutSecHeading">Price: {prodp.price * prodp.ct}</h2>
             <div className="fCartExt">Qnt: {prodp.ct}</div>
           </div>
 
-          <button className="secButton" onClick={() => removeToCart(prodp)}>
+          <button className="secButton DisableCss" onClick={() => removeToCart(prodp)}>
             Remove
           </button>
         </div>
+
+        <button style={{display:"inline", marginInline:"auto", marginBottom: "2%"}} className="secButton disableCss" onClick={() => removeToCart(prodp)}>
+            Remove
+          </button>
       </div>
     );
   });
@@ -70,6 +74,7 @@ const handleAddToCart = () =>{
           {/* to proceed for chekout  */}
 
           <button
+            style={{display: "inline", marginInline:"auto"}}
             className="primButton"
             onClick={handleAddToCart}
             >
