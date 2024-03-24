266ecbf4c31d6e1260761c1343fceb4aec27fb7f
tnavneet8628@gamil.com <tnavneer8628@gmail.com>
2024-03-24 23:49:19 +0530

    review part completed

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import heart from "../photos/heart.png";
 import LoadIndicator from "./LoadIndicator";
 import ProdPageContext from "../contexts/ProdPageContext";
@@ -7,13 +7,16 @@ import { fadd, fremove } from "../store/FavoriteSlice";
 import { useDispatch } from "react-redux";
 import Navbar from "./Navbar";
 import { useNavigate } from "react-router-dom";
-import ReactStars from "react-rating-stars-component";
+import Rating from "react-rating";
+import { FaStar } from "react-icons/fa";
+import { FaRegStar } from "react-icons/fa";
+import sampleImg from "../photos/avtar.svg";
+import { GiCancel } from "react-icons/gi";
 
 export default function ProductPage() {
   const { prodp, load } = useContext(ProdPageContext);
   const [mainimg, setMainimg] = useState(prodp.images[0]);
   let dummyimg;
-  let url = process.env.REACT_APP_MY_IP;
 
   // for navigation
   const navigate = useNavigate();
@@ -48,6 +51,7 @@ export default function ProductPage() {
     console.log(updatedProd);
     dispatch(add(updatedProd));
 
+    let url = process.env.REACT_APP_MY_IP;
     url += "/cart/addToCart";
     const response = await fetch(url, {
       method: "POST",
@@ -76,6 +80,7 @@ export default function ProductPage() {
     const updatedProd = { prod: prodp, ct: ct };
     dispatch(fadd(updatedProd));
 
+    let url = process.env.REACT_APP_MY_IP;
     url += "/cart/addToFav";
     const response = await fetch(url, {
       method: "POST",
@@ -94,6 +99,93 @@ export default function ProductPage() {
     }
   };
 
+  // ----------fetching reviews ---------------
+  const [rating, setRating] = useState([]);
+  const [totRating, setTotRating] = useState(parseInt(5));
+  let tempRating = [];
+
+  const fetchReviews = async () => {
+    const url = `${process.env.REACT_APP_MY_IP}/rating/getRating`;
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ product_id: prodp._id }),
+    });
+
+    const res = await response.json();
+    setRating(() => {return res.rating;});
+    tempRating = res.rating;
+  };
+
+  // stating up the total rating
+  const countRating = () => {
+    let temp = 5;
+    for (let i of tempRating) {
+      temp = temp + parseInt(i.rate);
+    }
+    temp = temp / (tempRating.length + 1);
+    setTotRating(() => {
+      return parseInt(temp);
+    });
+  };
+
+  useEffect(() => {
+    fetchReviews();
+    setTimeout(() => {
+      countRating();
+    }, 1000);
+  }, []);
+
+  // ---------------Reivew box related logic ---------
+  // to hold values of review related data (enterd by user )
+  const [userReview, setUserReview] = useState({
+    rate: 0,
+    desc: "",
+  });
+
+  const handleRateChange = (value) =>{
+        setUserReview({...userReview, rate: value});
+  }
+  const handleDescChange = (event) =>{
+        setUserReview({...userReview, desc: event.target.value});
+  }
+
+  // to toggle add review box
+  const toggleReviewBlock = () => {
+    document.getElementById("reviewBlock").classList.toggle("disableCss");
+  };
+
+  //to submit review
+  const addReview = async () => {
+    const url = `${process.env.REACT_APP_MY_IP}/rating/addReview`;
+    const response = await fetch(url,
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        custmrtoken: localStorage.getItem("custmrtoken"),
+      },
+      body: JSON.stringify({ product_id: prodp._id, rate: userReview.rate, desc: userReview.desc }),
+    });
+
+    const result = await response.json();
+    if(result.signal === "red") {
+      alert(`internal server error: ${result.error || "please try later"}`);
+      toggleReviewBlock();
+      return;
+    }
+    else{
+      alert("review added");
+    }
+    fetchReviews();
+    setTimeout(() => {
+      countRating();
+    }, 1000);
+    toggleReviewBlock();
+  };
+  
   // -------------actual component to be returned---------------------
   return (
     <>
@@ -118,6 +210,7 @@ export default function ProductPage() {
                 {prodp.images.map((image) => {
                   return (
                     <img
+                      key={image}
                       src={`${process.env.REACT_APP_MY_IP}/${image}`.replace(
                         /\\/g,
                         "/"
@@ -138,13 +231,34 @@ export default function ProductPage() {
               <h1>{prodp.title} - leatest phone in the market</h1>
 
               {/* rating division  */}
-              <ReactStars
-                value={3}
-                count={5}
-                size={35}
-                activeColor="#ffd700"
-                edit={false}
-              />
+              <div
+                style={{
+                  zIndex: "-1",
+                  display: "flex",
+                  alignItems: "center",
+                  gap: "5%",
+                }}
+              >
+                <Rating
+                  initialRating={totRating}
+                  emptySymbol={<FaRegStar size={30} />}
+                  fullSymbol={<FaStar style={{ color: "#e8e337" }} size={30} />}
+                />
+
+                <button
+                  onClick={() => alert(totRating)}
+                  style={{
+                    color: "orangered",
+                    fontSize: "13px",
+                    border: "1px solid orangered",
+                    borderRadius: "2px",
+                    padding: "3px",
+                    cursor: "pointer",
+                  }}
+                >
+                  all ratings &darr;
+                </button>
+              </div>
 
               {/* overview division */}
               <div className="flexRow">
@@ -179,6 +293,7 @@ export default function ProductPage() {
                   style={{ width: "40vw" }}
                   className="disableCss DisBlockCss primButton"
                   onClick={handleAddToCart}
+                  // onClick={() => alert(totRating)}
                 >
                   Add To Cart
                 </button>
@@ -197,6 +312,117 @@ export default function ProductPage() {
           <div id="pDesc" className="container flexCol">
             <h2>Description :-</h2>
             <h3>{prodp.desc}</h3>
+          </div>
+
+          {/* Reviews  */}
+          <div
+            style={{
+              border: "2px solid lightgray",
+              padding: "5%",
+            }}
+            className="container flexCol"
+          >
+            <h2>Customer reviews</h2>
+            <div
+              style={{
+                minHeight: "50%",
+                marginTop: "3%",
+              }}
+            >
+              {rating?.map((review) => {
+                return (
+                  <div
+                    className="flexRow"
+                    style={{ marginTop: "2%" }}
+                    key={review}
+                  >
+                    <div
+                      style={{
+                        border: "1px solid blue",
+                        height: "50px",
+                        width: "50px",
+                        borderRadius: "50%",
+                        overflow: "hidden",
+                      }}
+                    >
+                      <img src={sampleImg} height="100%" width="100%" alt="" />
+                    </div>
+                    <div className="flexCol" style={{ marginLeft: "3%" }}>
+                      <h4>{review.name}</h4>
+                      <div style={{ marginTop: "2%" }}>
+                        <Rating
+                          readonly
+                          initialRating={review.rate}
+                          emptySymbol={<FaRegStar size={20} />}
+                          fullSymbol={
+                            <FaStar style={{ color: "#e8e337" }} size={20} />
+                          }
+                        />
+                      </div>
+                      <div>{review.desc}</div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+
+            {/* to add review */}
+            <button className="primButton" onClick={toggleReviewBlock}>
+              Add review
+            </button>
+          </div>
+          <div
+            id="reviewBlock"
+            className="disableCss"
+            style={{
+              paddingLeft: "5%",
+              borderRadius: "8px",
+              position: "fixed",
+              top: 150,
+              left: 200,
+              height: "500px",
+              width: "70%",
+              zIndex: "100000",
+              boxShadow: "2px 2px 500px black",
+              backgroundColor: "white",
+            }}
+          >
+            <GiCancel
+              style={{ cursor: "pointer", marginLeft: "95%", marginTop: "2%" }}
+              color="royalblue"
+              size={25}
+              onClick={toggleReviewBlock}
+            />
+            <div>
+              <h2 style={{ marginBottom: "1%" }}>How you rate this Product?</h2>
+              <Rating
+                initialRating={userReview.rate}
+                emptySymbol={<FaRegStar size={34} />}
+                fullSymbol={<FaStar size={35} style={{ color: "#e8e337" }} />}
+                onChange={handleRateChange}
+              />
+            </div>
+            <div style={{ marginTop: "3%" }}>
+              <h2 style={{ marginBottom: "1%" }}>
+                Share your valueable experience with us?
+              </h2>
+              <textarea
+              name="desc"
+              value={userReview.desc}
+                style={{ padding: "8px" }}
+                placeholder="Very useful product"
+                rows={10}
+                cols={55}
+                onChange={handleDescChange}
+              />
+            </div>
+            <button
+              style={{ marginTop: "3%" }}
+              className="primButton"
+              onClick={addReview}
+            >
+              Submit
+            </button>
           </div>
         </>
       )}
